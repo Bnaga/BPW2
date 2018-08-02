74c696251a9d921419684672288d8461981f41c2
Unknown <BayanganNaga@hotmail.com>
2018-08-02 22:06:24 +0200

    basic movement

---
+++ BPW2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float moveSpeed;
+	public Rigidbody rigid;
+	public float jumpForce;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		rigid.velocity = new Vector3(Input.GetAxis("Horizontal")*moveSpeed,rigid.velocity.y,Input.GetAxis("Vertical")*moveSpeed);
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			rigid.velocity = new Vector3(rigid.velocity.x,jumpForce,rigid.velocity.z);
+		}
+	}
+	
+}
