a823792201504cbe81b9252e798e3e08bd5001c7
Unknown <BayanganNaga@hotmail.com>
2018-08-07 02:07:48 +0200

    improved camera and movement

--- BPW2/Assets/Scripts/CameraController.cs
+++ BPW2/Assets/Scripts/CameraController.cs
@@ -11,6 +11,13 @@ public class CameraController : MonoBehaviour
 	public Vector3 offset;
 
 	public bool useOffsetValues;
+
+	public float rotateSpeed;
+
+	public Transform pivot;
+
+	public bool lockMode = false;
+	
 	// Use this for initialization
 	void Start ()
 	{
@@ -18,13 +25,38 @@ public class CameraController : MonoBehaviour
 		{
 			offset = target.position - transform.position;
 		}
+
+		pivot.transform.position = target.transform.position;
+		pivot.transform.parent = target.transform;
+
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void LateUpdate ()
 	{
+		
+		//get the x position of the mouse & rotate the target
+		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed; 
+		target.Rotate(0, horizontal,0);
 
-		transform.position = target.position - offset;
+		//get the x position of the mouse & rotate the pivot
+		float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+		pivot.Rotate(-vertical,0,0);
+		
+		//move the camera based on current rotation of target
+		float desiredYAngle = target.eulerAngles.y;
+		float desiredXAngle = pivot.eulerAngles.x;
+		Quaternion rotation = Quaternion.Euler(desiredXAngle,desiredYAngle,0);
+
+		transform.position = target.position - (rotation * offset);
+
+		if (transform.position.y < target.position.y)
+		{
+			transform.position = new Vector3(transform.position.x, target.position.y -.5f, transform.position.z);
+		}
+		
+		//transform.position = target.position - offset;
 		transform.LookAt(target.position+targetOffset);
 		
 	}
--- BPW2/Assets/Scripts/PlayerController.cs
+++ BPW2/Assets/Scripts/PlayerController.cs
@@ -33,17 +33,22 @@ public class PlayerController : MonoBehaviour
 		}
 		*/
 		
-		moveDirection = new Vector3(Input.GetAxis("Horizontal")*moveSpeed, moveDirection.y,Input.GetAxis("Vertical")*moveSpeed);
-
+		//moveDirection = new Vector3(Input.GetAxis("Horizontal")*moveSpeed, moveDirection.y,Input.GetAxis("Vertical")*moveSpeed)
+		float yStore = moveDirection.y;
+		moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+		moveDirection = moveDirection.normalized * moveSpeed;
+		moveDirection.y = yStore;
 		if (controller.isGrounded)
 		{
 			//moveDirection = new Vector3(Input.GetAxis("Horizontal")*moveSpeed,0f,Input.GetAxis("Vertical")*moveSpeed);
+			moveDirection.y = 0f;
 			
 			if (Input.GetButtonDown("Jump"))
 			{
 				moveDirection.y = jumpForce;
 			}
-			moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale/50 * Time.deltaTime);
+
+			
 		}
 
 		if (!controller.isGrounded)
